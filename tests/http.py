973a627dcf86e3be3303e73ff0e2f08070aec7f5
phpdude <alexandr.shurigin@gmail.com>
2016-01-22 05:29:31 +0200

    added tests for http

--- tests/http.py
+++ tests/http.py
@@ -17,24 +17,21 @@ class TestApi(unittest.TestCase):
         cls.auth_token_args = ('veu2n74k01', '4ed3df60fc9d11e3a3ac0800200c9a66')
 
     def pool_close(self):
-        def wrapper(func, *args, **kwargs):
-            func(*args, **kwargs)
-
-            self.client.rest_client.pool_manager.close()
-
-        return wrapper
+        """
+        Close pool to test connect timeout and read timeout settings.
+        """
+        self.client.rest_client.pool_manager.close()
 
     def test_connect_timeout(self):
         old = self.config.http_timeout_connect
+
         self.config.http_timeout_connect = 0.001
-        # self.config.http_timeout_read = 0.001
-
-        # self.client.get_auth_token(*self.auth_token_args)
-
         with self.assertRaises(ConnectTimeout):
             self.client.get_auth_token(*self.auth_token_args)
 
         self.config.http_timeout_connect = old
+
+        self.pool_close()
 
     def test_read_timeout(self):
         old = self.config.http_timeout_read
@@ -45,13 +42,24 @@ class TestApi(unittest.TestCase):
 
         self.config.http_timeout_read = old
 
+        self.pool_close()
+
     def test_user_agent(self):
-        self.client.get_auth_token(*self.auth_token_args)
+        if not hasattr(self.client, 'last_response'):
+            self.client.get_auth_token(*self.auth_token_args)
+
         self.assertEqual(self.client.last_response.urllib3_response.request.headers['User-Agent'],
                          self.client.user_agent)
 
-    @self.pool_close
+        self.pool_close()
+
     def test_compression(self):
-        self.client.get_auth_token(*self.auth_token_args)
+        if not hasattr(self.client, 'last_response'):
+            self.client.get_auth_token(*self.auth_token_args)
+
+        resp = self.client.last_response.urllib3_response
+
+        self.assertIn('gzip', (resp.request.headers['Accept-Encoding'] or "").lower())
+        self.assertIn('gzip', (resp.headers['Content-Encoding'] or "").lower())
 
         self.pool_close()

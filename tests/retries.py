eca41a9392ce7e005db778a75356c642fc961545
phpdude <alexandr.shurigin@gmail.com>
2016-01-26 19:36:01 +0200

    added more retries tests

--- tests/retries.py
+++ tests/retries.py
@@ -3,6 +3,7 @@ import unittest
 from requests.exceptions import RetryError
 
 from attune.client.client import Client
+from attune.client.rest import ApiException
 from tests.utils import Server
 
 
@@ -27,10 +28,28 @@ class TestRetries(unittest.TestCase):
         self.assertIsNotNone(result)
         self.assertEqual(result.data, 'It works!')
 
-    def test_500_error_retries(self):
+    def test_500_errors_pass_with_retries(self):
         self.server.reset_errors_count()
 
         try:
             self.client.rest_client.GET(self.base_url % ('/error/%s' % range(500, 600)))
         except RetryError:
             self.assertEqual(self.client.config.http_max_retries + 1, self.server.errors_count)
+
+    def test_400_errors_pass_without_retries(self):
+        for code in range(400, 405):
+            self.server.reset_errors_count()
+
+            try:
+                self.client.rest_client.GET(self.base_url % ('/error/%s' % code))
+            except ApiException:
+                self.assertEqual(self.server.errors_count, 1)
+
+    def test_300_error_pass_without_retries(self):
+        for code in range(300, 305):
+            self.server.reset_errors_count()
+
+            try:
+                self.client.rest_client.GET(self.base_url % ('/error/%s' % code))
+            except ApiException:
+                self.assertEqual(self.server.errors_count, 1)

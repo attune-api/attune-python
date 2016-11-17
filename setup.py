4ca270081ea61d0149d5b4f7b080c466794db2ef
Jose Thomas <jthomas@attune.co>
2016-11-17 14:01:36 -0800

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='attune-python',
-        version="1.0.3",
+        version="1.0.4",
         description="Attune.co API Client",
         author_email="python@attune.co",
         author='Attune.co',
